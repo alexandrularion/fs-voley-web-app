f931ee3300cee424d7a033129de279d1f6a60cf2
CatalinStratu <catalinstratu45@gmail.com>
2023-01-25 17:21:52 +0200

    Change error status code

--- pages/api/sponsors/create.ts
+++ pages/api/sponsors/create.ts
@@ -27,6 +27,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
   } catch (e) {
     console.log(e);
-    return res.status(404).json({ message: e });
+    return res.status(500).json({ message: e });
   }
 }
