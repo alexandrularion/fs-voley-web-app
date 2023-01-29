f05958dbf193a1eac384ec16b0cafa2ceaa2d020
CatalinStratu <catalinstratu45@gmail.com>
2023-01-29 20:54:06 +0200

    Add createdAt

--- pages/api/player/create.ts
+++ pages/api/player/create.ts
@@ -1,7 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 import { getSession } from 'next-auth/react';
-
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb', // Set desired value here
+    },
+  },
+};
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method == 'POST') {
