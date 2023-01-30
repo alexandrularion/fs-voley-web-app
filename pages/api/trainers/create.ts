71d045b93ccb7198425e7a39be8bc2d97f952479
CatalinStratu <catalinstratu45@gmail.com>
2023-01-30 21:43:22 +0200

    Add create trainer

--- pages/api/trainers/create.ts
+++ pages/api/trainers/create.ts
@@ -1,8 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '../../../../lib/prisma';
+import prisma from '../../../lib/prisma';
 import { getSession } from 'next-auth/react';
-export const config = { api: { bodyParser: false } };
-
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '4mb', // Set desired value here
+    },
+  },
+};
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     if (req.method == 'POST') {
@@ -10,12 +15,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       if (session == null) {
         return res.status(401).json({ message: 'Unauthorized' });
       }
-
-      const { id } = req.query;
       const { first_name, last_name, description, image } = req.body;
-
-      await prisma.trainers.update({
-        where: { id: Number(id) },
+      const user = await prisma.trainers.create({
         data: {
           first_name: first_name,
           last_name: last_name,
@@ -23,12 +24,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
           image: image,
         },
       });
-      return res.status(200).json({ message: 'The trainer was updated!' });
+
+      return res.status(200).json(user);
     } else {
-      res.status(405).json({ message: 'Method Not Allowed' });
+      return res.status(405).json({ message: 'Method Not Allowed' });
     }
   } catch (e) {
     console.log(e);
-    res.status(404).json({ message: e });
+    return res.status(500).json({ message: e });
   }
 }
