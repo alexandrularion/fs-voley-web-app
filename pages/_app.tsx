20cb98ae7420cfe24938c105cf3e8d86b2f746c4
alexandrularion <larion.a@yahoo.com>
2023-02-05 20:34:29 +0200

    Added progress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,12 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { ToastContainer } from 'react-toastify';
 import { SessionProvider } from 'next-auth/react';
 import { AppContextProvider } from '../context/ContextProvider';
+import NextNProgress from 'nextjs-progressbar';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <AppContextProvider>
+      <NextNProgress {...{ showOnShallow: true, color: 'var(--blue-600)', height: 3 }} />
       <SessionProvider {...{ session, refetchOnWindowFocus: true, refetchInterval: 3000 }}>
         <ChakraProvider>
           <Component {...pageProps} />
